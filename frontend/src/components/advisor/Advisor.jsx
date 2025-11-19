7298cf4e16e27c42e870ce5b8b241dffc9675c56
Adriana Zoraya Flórez Sandoval <141433571+ZorayaF@users.noreply.github.com>
2025-11-18 19:05:48 -0500

    Configurar URL producción

--- frontend/src/components/advisor/Advisor.jsx
+++ frontend/src/components/advisor/Advisor.jsx
@@ -41,7 +41,7 @@ export default function Advisor() {
     try {
       // 2. Conexión con tu Backend Python (FastAPI)
       // Asegúrate de que el servidor uvicorn esté corriendo en este puerto
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://bytestep.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
